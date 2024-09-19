33c972630c3069dc1f22bcd72acb535f2de36d9b
esnayder aguilar <aguilarcancahchij@iestptrujillo.net>
2024-09-19 11:01:45 -0500

    mi primer comit

---
+++ Controllers/Alumno.cs
@@ -0,0 +1,46 @@
+﻿namespace WebApi.Controllers
+{
+    public class Alumno
+    {
+        //creando propiedades
+        string nombre;
+        string apellido;
+        string dni;
+        DateTime fechanacimiento;
+
+        //constructor sin parametros 
+        public Alumno()
+        {
+            nombre = string.Empty;
+            apellido = string.Empty;
+            dni = string.Empty;
+            fechanacimiento = DateTime.Now;
+        }
+        //construcutor con parametros
+        public Alumno(string pnombre, string papellido, string pdni)
+        {
+            nombre = pnombre;
+            apellido = papellido;
+            dni = pdni;
+        }
+
+        public Alumno(string pnombre, string papellido, string pdni, DateTime pfechanacimiento)
+        {
+            nombre = pnombre;
+            apellido = papellido;
+            dni = pdni;
+            fechanacimiento = pfechanacimiento;
+        }
+
+        public string DevolverEDad()
+        {
+            return dni;
+        }
+
+        public int DevolverEdad()
+        {
+            return DateTime.Now.Year - fechanacimiento.Year;
+        }
+
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("DevolverEdad")]
+        public int DevolverEdad()
+        {
+            string mensaje = string.Empty;
+            Alumno Alumno3 = new Alumno("Alexandra", "Meregildo", "81560608", new DateTime(2006, 1, 31));
+            return Alumno3.DevolverEdad();
+        }
+    }
+}
